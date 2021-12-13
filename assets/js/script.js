dd35f068cfdc2ba4422bd85672ec61e43491e833
Felipe Zintzun <felipemendoza@felipes-mbp.lan>
2021-12-12 20:13:28 -0600

    cleaned up clear buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,63 +6,24 @@ let update = function () {
   };
   setInterval(update, 1000);
   
-  // CLEAR BUTTONS 
-  $("#9AMClearBTN").click(function () {
-    $("#9").val("");
-    localStorage.setItem(9, "");
-  });
-  
-  $("#10AMClearBTN").click(function () {
-    $("#10").val("");
-    localStorage.setItem(10, "");
-  });
-  
-  $("#11AMClearBTN").click(function () {
-    $("#11").val("");
-    localStorage.setItem(11, "");
-  });
-  
-  $("#12PMClearBTN").click(function () {
-    $("#12").val("");
-    localStorage.setItem(12, "");
-  });
-  
-  $("#1PMClearBTN").click(function () {
-    $("#13").val("");
-    localStorage.setItem(13, "");
-  });
-  
-  $("#2PMClearBTN").click(function () {
-    $("#14").val("");
-    localStorage.setItem(14, "");
-  });
-  
-  $("#3PMClearBTN").click(function () {
-    $("#15").val("");
-    localStorage.setItem(15, "");
-  });
-  
-  $("#4PMClearBTN").click(function () {
-    $("#16").val("");
-    localStorage.setItem(16, "");
-  });
-
-  $("#5PMClearBTN").click(function () {
-    $("#17").val("");
-    localStorage.setItem(17, "");
-  });
-  
-  // SAVE BUTTONS
-  
   $(document).ready(function () {
     let timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let currentHour = moment().hour()
+    console.log(currentHour)
     function renderPlans() {
       for (let i = 0; i <= timeSlots.length; i++) {
         $("#" + timeSlots[i]).val(localStorage.getItem(timeSlots[i]));
+
+        // CHANGE COLORS DEPENDING ON HOUR
+        if (timeSlots[i] < currentHour ){
+          $("#" + timeSlots[i]). addClass("past")
+        } else if (timeSlots[i] === currentHour){
+          $("#" + timeSlots[i]). addClass("present")
+        } else {$("#" + timeSlots[i]). addClass("future")}
       }
     }
     renderPlans();
-  
+    // SAVE BUTTON
     $(".saveBtn").click(function () {
       let dataHour = $(this).attr("data-hour");
       let inputField = $("#" + dataHour).val();
@@ -70,7 +31,14 @@ let update = function () {
       localStorage.setItem(dataHour, inputField);
       console.log(inputField);
     });
+    // CLEAR BUTTON
+    $(".clearBtn").click(function () {
+      let dataHour = $(this).attr("data-hour");
+        $("#" + dataHour).val("");
+        localStorage.setItem(dataHour, "");
+      });
+
+
   });
   
-  // CHANGE COLORS 
 
